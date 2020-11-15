04e69f8f891dac83d4ed9a9030fb8e0c91540ff0
leplasmo <marcel@leplasmo.be>
2020-11-15 19:06:26 +0100

    boilerplate empty project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/** @format */
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
 });
